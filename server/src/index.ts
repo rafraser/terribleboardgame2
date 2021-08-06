227d53492402820c1930290df7ecfa14a26f48c3
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-08-07 02:32:05 +1000

    Tidy up server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,17 @@
 import express from 'express';
-import http from 'http';
 import cors from 'cors';
 import socketInit from './socket';
 
 import api from './api';
 
-// Setup Express application to serve the app/ directory
+// Setup Express
 const app = express();
+app.use(cors());
+app.use('/api/', api);
 app.use(express.static(`${__dirname}/app`));
 
-// Register API endpoints
-app.use('/api/', api);
-
-// Use CORS in development
-app.use(cors());
-
 // Create an HTTP server and pass to the socket init
-const server = http.createServer(app);
-socketInit(server);
-
-server.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('Server started on *:3000');
 });
+socketInit(server);
